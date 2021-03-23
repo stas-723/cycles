d97d67d7bb924b0b57490e338e20889fd4df43e1
stas <stas-723@ya.ru>
2021-03-23 03:51:15 +0300

    HomeWork2

---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % a == 0)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+
+
+
+        }
+    }
+}
