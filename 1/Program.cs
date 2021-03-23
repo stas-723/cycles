e25281de0296613afe3b17ef409351345a8b3a0f
stas <stas-723@ya.ru>
2021-03-23 03:47:16 +0300

    HomeWork1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+
+            Console.WriteLine($"A в степени B = {result}");
+
+
+        }
+    }
+}
